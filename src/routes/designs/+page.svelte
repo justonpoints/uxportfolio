e82341f2175c44cc7b055c905e0b1e3bbfb10c44
Juston Points <justonpoints@gmail.com>
2023-03-16 23:48:04 -0700

    fixed issue with missing route pages

---
+++ src/routes/designs/+page.svelte
@@ -0,0 +1,79 @@
+<svelte:head>
+	<title>Test</title>
+	<meta name="description" content="This is a test page" />
+</svelte:head>
+
+<div class="text-column">
+	<div class="container mx-auto bg-red-200 rounded-xl shadow border p-8 m-10">
+		<p class="text-3xl text-black-700 font-bold mb-5">
+		Welcome!
+		</p>
+		<p class="text-gray-500 text-lg">
+		SvelteKit and Tailwind CSS in action
+		</p>
+	</div>
+		<div class="container mx-auto bg-red-200 rounded-xl shadow border p-8 m-10">
+		<p class="text-3xl text-black-700 font-bold mb-5">
+		Welcome!
+		</p>
+		<p class="text-gray-500 text-lg">
+		SvelteKit and Tailwind CSS in action
+		</p>
+	</div>
+		<div class="container mx-auto bg-red-200 rounded-xl shadow border p-8 m-10">
+		<p class="text-3xl text-black-700 font-bold mb-5">
+		Welcome!
+		</p>
+		<p class="text-gray-500 text-lg">
+		SvelteKit and Tailwind CSS in action
+		</p>
+	</div>
+		<div class="container mx-auto bg-red-200 rounded-xl shadow border p-8 m-10">
+		<p class="text-3xl text-black-700 font-bold mb-5">
+		Welcome!
+		</p>
+		<p class="text-gray-500 text-lg">
+		SvelteKit and Tailwind CSS in action
+		</p>
+	</div>
+		<div class="container mx-auto bg-red-200 rounded-xl shadow border p-8 m-10">
+		<p class="text-3xl text-black-700 font-bold mb-5">
+		Welcome!
+		</p>
+		<p class="text-gray-500 text-lg">
+		SvelteKit and Tailwind CSS in action
+		</p>
+	</div>
+		<div class="container mx-auto bg-red-200 rounded-xl shadow border p-8 m-10">
+		<p class="text-3xl text-black-700 font-bold mb-5">
+		Welcome!
+		</p>
+		<p class="text-gray-500 text-lg">
+		SvelteKit and Tailwind CSS in action
+		</p>
+	</div>
+		<div class="container mx-auto bg-red-200 rounded-xl shadow border p-8 m-10">
+		<p class="text-3xl text-black-700 font-bold mb-5">
+		Welcome!
+		</p>
+		<p class="text-gray-500 text-lg">
+		SvelteKit and Tailwind CSS in action
+		</p>
+	</div>
+		<div class="container mx-auto bg-red-200 rounded-xl shadow border p-8 m-10">
+		<p class="text-3xl text-black-700 font-bold mb-5">
+		Welcome!
+		</p>
+		<p class="text-gray-500 text-lg">
+		SvelteKit and Tailwind CSS in action
+		</p>
+	</div>
+		<div class="container mx-auto bg-red-200 rounded-xl shadow border p-8 m-10">
+		<p class="text-3xl text-black-700 font-bold mb-5">
+		Welcome!
+		</p>
+		<p class="text-gray-500 text-lg">
+		SvelteKit and Tailwind CSS in action
+		</p>
+	</div>
+</div>
